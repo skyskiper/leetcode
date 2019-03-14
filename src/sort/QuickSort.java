fefe01163bc40477e1f32297e4c76a27564eea18
summer <sum920229@gmail.com>
2019-03-14 17:26:01 +0800

    快排之不左小于右大于等于

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,59 @@
+package sort;
+
+
+public class QuickSort {
+
+    private static int partition(int[] nums, int start, int end, int target) {
+        while (start < end) {
+            while (nums[start] < target) {
+                start++;
+                if (start >= end) {
+                    return end;
+                }
+            }
+            while (nums[end] >= target) {
+                end--;
+                if (start >= end) {
+                    return end;
+                }
+            }
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+        }
+
+        return end;
+    }
+
+    private static void quickSort(int[] arr, int left, int right) {
+        if (arr == null || left >= right || arr.length <= 1)
+            return;
+        int tmp = arr[left];
+        int mid = partition(arr, left, right, arr[left]);
+        for (int i = left; i <= right; i++) {
+            System.out.print(arr[i] + "    ");
+            if (i == mid - 1) {
+                System.out.print("   split   ");
+            }
+        }
+        System.out.println("hello-" + mid + "target-" + tmp);
+        if (mid == left) {
+            quickSort(arr, mid + 1, right);
+        } else {
+            quickSort(arr, left, mid - 1);
+            quickSort(arr, mid, right);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] num = new int[]{-1, -2, -3, 4, 1, 3, 0, 3, -2, 1, -2, 2, -1, 1, -5, 4, -3};
+        quickSort(num, 0, num.length - 1);
+        System.out.println("-------");
+        System.out.println("result");
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i] + "    ");
+        }
+
+    }
+}
