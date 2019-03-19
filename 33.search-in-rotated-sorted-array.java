dc449212ec9819677c5eedb582e93d51c70a09a7
summer <sum920229@gmail.com>
2019-03-19 14:16:37 +0800

    33. Search in Rotated Sorted Array

--- 33.search-in-rotated-sorted-array.java
+++ 33.search-in-rotated-sorted-array.java
@@ -1,9 +1,42 @@
-package test;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+/*
+ * @lc app=leetcode id=33 lang=java
+ *
+ * [33] Search in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (32.67%)
+ * Total Accepted:    379.5K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * 
+ * You are given a target value to search. If found in the array return its
+ * index, otherwise return -1.
+ * 
+ * You may assume no duplicate exists in the array.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ * 
+ */
 class Solution {
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
@@ -41,39 +74,5 @@ class Solution {
             return binarySearch(nums, mid + 1, right, target);
         }
         return binarySearch(nums, left, mid - 1, target);
-    }
-}
-
-public class MainClass {
-    public static int[] stringToIntegerArray(String input) {
-        input = input.trim();
-        input = input.substring(1, input.length() - 1);
-        if (input.length() == 0) {
-            return new int[0];
-        }
-
-        String[] parts = input.split(",");
-        int[] output = new int[parts.length];
-        for(int index = 0; index < parts.length; index++) {
-            String part = parts[index].trim();
-            output[index] = Integer.parseInt(part);
-        }
-        return output;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        while ((line = in.readLine()) != null) {
-            int[] nums = stringToIntegerArray(line);
-            line = in.readLine();
-            int target = Integer.parseInt(line);
-
-            int ret = new Solution().search(nums, target);
-
-            String out = String.valueOf(ret);
-
-            System.out.print(out);
-        }
     }
 }
